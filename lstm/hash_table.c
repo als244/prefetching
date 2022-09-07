8e02ba95777349cf0a57319c9f8d34e866e3e1ff
Andrew Sheinberg <andrew.sheinberg@gmail.com>
2022-09-06 22:37:06 -0600

    fixed buggy linked list functions

--- lstm/hash_table.c
+++ lstm/hash_table.c
@@ -11,54 +11,34 @@
 
 /* WILL USE HASH TABLE FOR LOOKUP FROM address delta -> encoding index */
 
-
-#define CAPACITY 50000 // Size of the Hash Table
-
 // found at "https://lemire.me/blog/2018/08/15/fast-strongly-universal-64-bit-hashing-everywhere/"
-long hash_function(long h)
+long hash_function(long h, int size)
 {
   h ^= h >> 33;
   h *= 0xff51afd7ed558ccdL;
   h ^= h >> 33;
   h *= 0xc4ceb9fe1a85ec53L;
   h ^= h >> 33;
-  return (h % CAPACITY);
+  return (h % size);
 }
 
-static LinkedList* allocate_list () {
-    // Allocates memory for a Linkedlist pointer
-    LinkedList* list = (LinkedList*) malloc (sizeof(LinkedList));
-    return list;
-}
 
-static LinkedList* linkedlist_insert(LinkedList* list, Ht_item* item) {
+LinkedList* linkedlist_insert(LinkedList* list, Ht_item* item) {
     // Inserts the item onto the Linked List
-    if (!list) {
-        LinkedList* head = allocate_list();
-        head->item = item;
-        head->next = NULL;
-        list = head;
-        return list;
-    } 
-    
-    else if (list->next == NULL) {
-        LinkedList* node = allocate_list();
-        node->item = item;
-        node->next = NULL;
-        list->next = node;
-        return list;
-    }
 
-    LinkedList* temp = list;
-    while (temp->next->next) {
-        temp = temp->next;
+    LinkedList * node = (LinkedList*) malloc (sizeof(LinkedList));
+    node -> item = item;
+    node -> next = NULL;
+    if (!list){
+        list = node;
     }
-    
-    LinkedList* node = allocate_list();
-    node->item = item;
-    node->next = NULL;
-    temp->next = node;
-    
+    else{
+        LinkedList * temp = list;
+        while (temp -> next){
+            temp = temp -> next;
+        }
+        temp -> next = node;
+    }    
     return list;
 }
 
@@ -82,7 +62,7 @@ static LinkedList* linkedlist_insert(LinkedList* list, Ht_item* item) {
 //     return it;
 // }
 
-static void free_linkedlist(LinkedList* list) {
+void free_linkedlist(LinkedList* list) {
     LinkedList* temp = list;
     while (list) {
         temp = list;
@@ -92,7 +72,7 @@ static void free_linkedlist(LinkedList* list) {
     }
 }
 
-static LinkedList** create_overflow_buckets(HashTable* table) {
+LinkedList** create_overflow_buckets(HashTable* table) {
     // Create the overflow buckets; an array of linkedlists
     LinkedList** buckets = (LinkedList**) calloc (table->size, sizeof(LinkedList*));
     for (int i=0; i<table->size; i++)
@@ -100,7 +80,7 @@ static LinkedList** create_overflow_buckets(HashTable* table) {
     return buckets;
 }
 
-static void free_overflow_buckets(HashTable* table) {
+void free_overflow_buckets(HashTable* table) {
     // Free all the overflow bucket lists
     LinkedList** buckets = table->overflow_buckets;
     for (int i=0; i<table->size; i++)
@@ -154,8 +134,9 @@ void handle_collision(HashTable* table, long index, Ht_item* item) {
 
     if (head == NULL) {
         // We need to create the list
-        head = allocate_list();
+        head = (LinkedList*) malloc (sizeof(LinkedList));
         head->item = item;
+        head->next = NULL;
         table->overflow_buckets[index] = head;
         return;
     }
@@ -171,7 +152,7 @@ void ht_insert(HashTable* table, long key, int value) {
     Ht_item* item = create_item(key, value);
 
     // Compute the index
-    long index = hash_function(key);
+    long index = hash_function(key, table->size);
 
     Ht_item* current_item = table->items[index];
     
@@ -208,7 +189,7 @@ void ht_insert(HashTable* table, long key, int value) {
 int ht_search(HashTable* table, long key) {
     // Searches the key in the hashtable
     // and returns NULL if it doesn't exist
-    long index = hash_function(key);
+    long index = hash_function(key, table->size);
     Ht_item* item = table->items[index];
     LinkedList* head = table->overflow_buckets[index];
 
@@ -255,7 +236,7 @@ void print_table(HashTable* table) {
 }
 
 // int main() {
-//     HashTable* ht = create_table(CAPACITY);
+//     HashTable* ht = create_table(4000);
 //     ht_insert(ht, 1, 10);
 //     ht_insert(ht, 2, 11);
 //     ht_insert(ht, 3, 12);
